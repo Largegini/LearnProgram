8bc4420e6aca0de362de1a00a06d755400bfea47
정연 안 <dkswjddus0215@gmail.com>
2022-04-09 12:59:47 +0900

    practice

--- CStep_08/Step_08/Main.cpp
+++ CStep_08/Step_08/Main.cpp
@@ -43,7 +43,6 @@ int main(void)
 	iNumber2 = 50;
 
 	printf_s("%d\n", *pPoint);
-	*/
 
 	// ** [포인터 사용 방법 3가지]
 	// ** 1. [*] = 데이터 [&] = 주소
@@ -62,9 +61,24 @@ int main(void)
 	printf_s("piNumber	: %d\n", piNumber);
 	printf_s("&iNumber	: %d\n", &iNumber);
 	printf_s("&piNumber	: %d\n", &piNumber);
+	*/
 
 	// 동적할당
 	// int* pNumber = (int*)malloc(sizeof(int));
+
+	printf_s("char		:%d\n", sizeof(char));
+	printf_s("short		:%d\n", sizeof(short));
+	printf_s("long		:%d\n", sizeof(long));
+	printf_s("int		:%d\n", sizeof(int));
+	printf_s("float		:%d\n", sizeof(float));
+	printf_s("double	:%d\n\n", sizeof(double));
+
+	printf_s("char*		:%d\n", sizeof(char*));
+	printf_s("short*	:%d\n", sizeof(short*));
+	printf_s("long*		:%d\n", sizeof(long*));
+	printf_s("int*		:%d\n", sizeof(int*));
+	printf_s("float*	:%d\n", sizeof(float*));
+	printf_s("double*	:%d\n", sizeof(double*));
 
 	return 0;
 }
