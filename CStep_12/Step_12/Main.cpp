8f481da7f13625af298354ad938e1340a68e272a
정연 안 <dkswjddus0215@gmail.com>
2022-04-21 20:52:45 +0900

    Step_13

--- CStep_12/Step_12/Main.cpp
+++ CStep_12/Step_12/Main.cpp
@@ -24,10 +24,12 @@ int SceneState;
 // 루프를 위한 전역 변수
 short Loop = 1;
 
-// Status 정보 구조체를 구축
+//구조체안에는 사용자가 사용하고 싶은 것을 넣어서 용도별로 구축한다
+
+// Status 구조체
 typedef struct tagInfo				
 {
-	char* Name;
+	
 	int HP;
 	int MP;
 
@@ -42,9 +44,10 @@ typedef struct tagInfo
 
 }INFO;
 
-//  객체 Status를 관리하기위한 구조체
+//  객체 구조체
 typedef struct tagObject		
 {
+	char* Name;
 	INFO Info;
 }Object;
 
@@ -65,11 +68,12 @@ void EnemyAttack();
 
 int main(void)
 {
-	
+	//Scene을 실행하기 위한 변수 초기화
 	SceneState = 0;
 	while (Loop)
 	{
-		SceneManager(SceneState);						// 변수 SceneState를 함수 SceneManger에 넣어 작동 한다
+		// 변수 SceneState를 함수 SceneManger에 넣어 작동 한다
+		SceneManager(SceneState);						
 	}
 	
 
@@ -84,7 +88,7 @@ void InitializeObject(Object* _obj, int ObjectType)
 	{
 		//PLAYER의 객체 정보를 초기화한다
 		case PLAYER:
-			_obj->Info.Name = SetName();		
+			_obj->Name = SetName();		
 			_obj->Info.HP = 100;
 			_obj->Info.MP = 10;
 			_obj->Info.Att = 10;
@@ -94,8 +98,9 @@ void InitializeObject(Object* _obj, int ObjectType)
 			_obj->Info.EXP = 0;
 			break;
 
+		//ENEMY의 객체정보를 초기화한다.
 		case ENEMY :
-			_obj->Info.Name = (char*)"Enemy";	//ENEMY의 객체정보를 초기화한다.
+			_obj->Name = (char*)"Enemy";	
 
 			_obj->Info.HP = 30;
 			_obj->Info.MP = 5;
@@ -112,15 +117,24 @@ void InitializeObject(Object* _obj, int ObjectType)
 //플레이어 이름을 설정하는 함수
 char* SetName()
 {
-	char Buffer[128] = "";								//문자열을 입력받기위한 임의의 공간 선언
+	//문자열을 입력받기위한 임의의 배열 선언
+	//포인터로는 문자열을 입력받을 수 없기 때문에 임의의 배열을 선언한다.
+	char Buffer[128] = "";								
 
 	printf_s("이름을 입력하십시오. : ");
+
+	//문자열을 입력 받음.
 	scanf("%s", Buffer);
 
-	char* pName = (char*)malloc(strlen(Buffer) + 1);	// 포인터의 동적할당
-	strcpy(pName, Buffer);								// 포인터에 문자열 복사
+	// ** 입력받은 값은 배열이나 반환 값은 캐릭터 포인터형(char*)이므로 문자열을 복사해야함.
+	// ** 문자열을 복사 하기위해 포인터가 가르키는 공간에 입력받은 문자열이 들어갈 만큼의 크기로 메모리 할당
+	char* pName = (char*)malloc(strlen(Buffer) + 1);	//문자열은 형변환을 해야함, NULL값을 넣어주기 위해 +1을 한다
 
-	return pName;										// 포인터 값 반환
+	// Buffer가 받은 문자열을 포인터pName에 문자열 복사
+	strcpy(pName, Buffer);								
+
+	// 포인터 값 반환
+	return pName;										
 }
 
 void SceneManager(int SceneState)						// Scene구성
@@ -130,12 +144,15 @@ void SceneManager(int SceneState)						// Scene구성
 	case Scene_Logo:
 		LogoScene();									// LogoScene 실행
 		break;
+
 	case Scene_Menu:
 		MenuScene();									// MenuScene 실행
 		break;
+
 	case Scene_Stage:									
 		StageScene();									// StageScene 실행
 		break;
+
 	case Scene_Exit:									//종료
 		Loop = 0;
 		break;
@@ -187,9 +204,8 @@ void StageScene()
 	Objects[ENEMY] = (Object*)malloc(sizeof(Object));	//ENEMY 동적할당
 	InitializeObject(Objects[ENEMY], ENEMY);			//초기화
 	
-	//무한반복하기위함
-	while (true)										
-	{
+	
+	
 		// 전투 반복을 위한 함수
 		short Battle = 1;								
 		Status();
@@ -237,7 +253,7 @@ void StageScene()
 						//주사위 굴리는 시스템 채용
 						if (rand() % 6 > 2)	
 						{
-							printf_s("%s(은)는 도망쳤다.\n", Objects[PLAYER]->Info.Name);
+							printf_s("%s(은)는 도망쳤다.\n", Objects[PLAYER]->Name);
 
 							//전투 종료 루프해제
 							Battle = 0;
@@ -252,7 +268,7 @@ void StageScene()
 						// 도망 실패 시
 						else
 						{
-							printf_s("%s는 도망치치 못했다.\n", Objects[PLAYER]->Info.Name);
+							printf_s("%s는 도망치치 못했다.\n", Objects[PLAYER]->Name);
 
 							// 도망치지 못했을 때 문구를 보이기위한 딜레이
 							Sleep(500);
@@ -267,7 +283,7 @@ void StageScene()
 
 					else
 					{
-						printf_s("%s는 도망치치 못했다.\n", Objects[PLAYER]->Info.Name);
+						printf_s("%s는 도망치치 못했다.\n", Objects[PLAYER]->Name);
 						Sleep(500);
 
 						EnemyAttack();
@@ -290,7 +306,7 @@ void StageScene()
 			}
 		}
 
-	}
+	
 }
 
 void Status()
@@ -298,7 +314,7 @@ void Status()
 	system("cls");
 
 	//플레이어의 정보를 출력
-	printf_s("[Player] : %s\n", Objects[PLAYER]->Info.Name);
+	printf_s("[Player] : %s\n", Objects[PLAYER]->Name);
 	printf_s("HP : %d\n", Objects[PLAYER]->Info.HP);
 	printf_s("MP : %d\n", Objects[PLAYER]->Info.MP);
 	printf_s("공격력 : %.2f\n", Objects[PLAYER]->Info.Att);
@@ -307,7 +323,7 @@ void Status()
 	printf_s("경험치 : %d\n", Objects[PLAYER]->Info.EXP);
 
 	//몬스터의 정보를 출력
-	printf_s("\n%s\n", Objects[ENEMY]->Info.Name);
+	printf_s("\n%s\n", Objects[ENEMY]->Name);
 	printf_s("HP : %d\n", Objects[ENEMY]->Info.HP);
 	printf_s("MP : %d\n", Objects[ENEMY]->Info.MP);
 	printf_s("공격력 : %.2f\n", Objects[ENEMY]->Info.Att);
@@ -319,10 +335,10 @@ void Status()
 
 void PlayerAttack()
 {
-	printf_s("%s의 공격!!\n", Objects[PLAYER]->Info.Name);
+	printf_s("%s의 공격!!\n", Objects[PLAYER]->Name);
 	//플레이어의 공격력과 몬스터의 방어력을 비교하여 데미지를 계산하는 조건문
 	if (Objects[PLAYER]->Info.Att > Objects[ENEMY]->Info.Def)
-		Objects[ENEMY]->Info.HP -= (Objects[PLAYER]->Info.Att - Objects[ENEMY]->Info.Def);
+		Objects[ENEMY]->Info.HP -= (int)(Objects[PLAYER]->Info.Att - Objects[ENEMY]->Info.Def);
 	// 방어력이 더 높을 경우 최소 1의 데미지를 주려고함
 	else
 		Objects[ENEMY]->Info.HP -= 1;
@@ -332,7 +348,7 @@ void EnemyAttack()
 {
 	printf_s("몬스터의 공격!!\n");
 	if (Objects[ENEMY]->Info.Att > Objects[PLAYER]->Info.Def)
-		Objects[PLAYER]->Info.HP -= (Objects[ENEMY]->Info.Att - Objects[PLAYER]->Info.Def);
+		Objects[PLAYER]->Info.HP -= (int)(Objects[ENEMY]->Info.Att - Objects[PLAYER]->Info.Def);
 	else
 		Objects[PLAYER]->Info.HP -= 1;
 }
